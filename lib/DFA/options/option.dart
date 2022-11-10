3e5c4b6ac51851987c9452bf6c139c8eff85e1f3
Parth Shah <shahparth200212@gmail.com>
2022-11-11 02:51:32 +0530

    Added testing functionality

--- lib/DFA/options/option.dart
+++ lib/DFA/options/option.dart
@@ -11,8 +11,13 @@ class OptionsMenu extends StatefulWidget {
 }
 
 class _OptionsMenuState extends State<OptionsMenu> {
+  bool enableflag = false;
   @override
   Widget build(BuildContext context) {
+    TextEditingController readinp = TextEditingController();
+    TextEditingController setout = TextEditingController();
+    TextEditingController outstate = TextEditingController();
+
     return Scaffold(
         appBar: AppBar(
           title: const Text("Operations on DFA"),
@@ -65,17 +70,59 @@ class _OptionsMenuState extends State<OptionsMenu> {
             Container(
               margin: const EdgeInsets.all(10),
               child: ListTile(
-                shape: RoundedRectangleBorder(
-                  side: BorderSide(color: Colors.black, width: 1),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                leading: Icon(Icons.account_circle),
-                title: Text('Test DFA'),
-                subtitle: Text("Custom test inputs for your DFA"),
-                onTap: () {
-                  print(widget.DFAobj.validate().toString());
-                },
-              ),
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(color: Colors.black, width: 1),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  leading: Icon(Icons.account_circle),
+                  title: Text('Test DFA'),
+                  subtitle: Text("Custom test inputs for your DFA"),
+                  onTap: () {
+                    AlertDialog alert = AlertDialog(
+                      title: Container(
+                          margin: EdgeInsets.fromLTRB(20, 5, 20, 1),
+                          child: Text('Enter Custom test Inputs')),
+                      content: Wrap(children: [
+                        Column(
+                          children: [
+                            TextField(
+                              controller: readinp,
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                  color: Colors.amber,
+                                )),
+                              ),
+                              style: const TextStyle(
+                                color: Colors.amber,
+                              ),
+                            ),
+                            TextField(
+                              controller: setout,
+                              enabled: enableflag,
+                            ),
+                            TextField(
+                              controller: outstate,
+                              enabled: enableflag,
+                            ),
+                          ],
+                        ),
+                      ]),
+                      actions: [
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text("Back")),
+                        ElevatedButton(
+                            onPressed: () {
+                              test(readinp, setout, outstate);
+                            },
+                            child: Text("Go"))
+                      ],
+                    );
+                    showDialog(context: context, builder: (context) => alert);
+                  }),
             ),
           ],
         ));
@@ -96,5 +143,23 @@ class _OptionsMenuState extends State<OptionsMenu> {
         Text("Validation Failure"),
       ],
     );
+  }
+
+  test(TextEditingController readinp, TextEditingController setout,
+      TextEditingController outstate) {
+    String userinput = readinp.text;
+    print(userinput);
+    setState(() {
+      enableflag = true;
+    });
+    try {
+      String exitstate = widget.DFAobj.testInput(userinput.split(''));
+      bool ans = widget.DFAobj.isAcceptingState(exitstate);
+      setout.text = (ans) ? "Decision : Accepted" : "Decision : Rejected";
+      outstate.text = "Custom input ended on state $exitstate";
+    } catch (e) {
+      setout.text = "Decision : Invalid input";
+      outstate.text = "Invalid symbol detected";
+    }
   }
 }
