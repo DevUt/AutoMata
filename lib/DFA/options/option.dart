2e1eb31cf43c2b0d003b3f252d20400b50e33e8b
Parth Shah <shahparth200212@gmail.com>
2022-11-11 23:44:13 +0530

    added synchronous updation of dialog box in step_wise transitions and test inputs

--- lib/DFA/options/option.dart
+++ lib/DFA/options/option.dart
@@ -11,7 +11,8 @@ class OptionsMenu extends StatefulWidget {
 }
 
 class _OptionsMenuState extends State<OptionsMenu> {
-  bool enableflag = false;
+  bool test_enableflag = false;
+  bool step_enableflag = false;
 
   @override
   Widget build(BuildContext context) {
@@ -68,50 +69,68 @@ class _OptionsMenuState extends State<OptionsMenu> {
                   subtitle:
                       Text("returns step wise transitions for each alphabet"),
                   onTap: () {
-                    AlertDialog alert = AlertDialog(
-                      title: Container(
-                          margin: EdgeInsets.fromLTRB(20, 5, 20, 1),
-                          child: Text(
-                              'Enter Custom test Input for step wise transitions')),
-                      content: Wrap(children: [
-                        Column(
-                          children: [
-                            TextField(
-                              controller: step_readinp,
-                              decoration: const InputDecoration(
-                                border: OutlineInputBorder(
-                                    borderSide: BorderSide(
-                                  color: Colors.amber,
-                                )),
-                              ),
-                              style: const TextStyle(
-                                color: Colors.amber,
-                              ),
-                            ),
-                            TextField(
-                              controller: step_setout,
-                            ),
-                            TextField(
-                              controller: step_outstates,
-                            ),
-                          ],
-                        ),
-                      ]),
-                      actions: [
-                        ElevatedButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            child: Text("Back")),
-                        ElevatedButton(
-                            onPressed: () {
-                              step_wise(
-                                  step_readinp, step_setout, step_outstates);
-                            },
-                            child: Text("Go"))
-                      ],
-                    );
-                    showDialog(context: context, builder: (context) => alert);
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return StatefulBuilder(builder: (context, setState) {
+                            return AlertDialog(
+                              title: Container(
+                                  margin: EdgeInsets.fromLTRB(20, 5, 20, 1),
+                                  child: Text(
+                                      'Enter Custom test Input for step wise transitions')),
+                              content: Wrap(children: [
+                                Column(
+                                  children: [
+                                    TextField(
+                                      controller: step_readinp,
+                                      decoration: const InputDecoration(
+                                        border: OutlineInputBorder(
+                                            borderSide: BorderSide(
+                                          color: Colors.amber,
+                                        )),
+                                      ),
+                                      style: const TextStyle(
+                                        color: Colors.amber,
+                                      ),
+                                    ),
+                                    Visibility(
+                                      visible: step_enableflag,
+                                      child: TextField(
+                                        controller: step_setout,
+                                        enabled: true,
+                                      ),
+                                    ),
+                                    Visibility(
+                                      visible: step_enableflag,
+                                      child: TextField(
+                                        controller: step_outstates,
+                                        enabled: true,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ]),
+                              actions: [
+                                ElevatedButton(
+                                    onPressed: () {
+                                      step_readinp.clear();
+                                      step_enableflag = false;
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text("Back")),
+                                ElevatedButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        step_enableflag = true;
+                                      });
+                                      step_wise(step_readinp, step_setout,
+                                          step_outstates);
+                                    },
+                                    child: Text("Go"))
+                              ],
+                            );
+                          });
+                        });
                   }),
             ),
             Container(
@@ -125,50 +144,67 @@ class _OptionsMenuState extends State<OptionsMenu> {
                   title: Text('Test DFA'),
                   subtitle: Text("Custom test inputs for your DFA"),
                   onTap: () {
-                    AlertDialog alert = AlertDialog(
-                      title: Container(
-                          margin: EdgeInsets.fromLTRB(20, 5, 20, 1),
-                          child: Text('Enter Custom test Inputs')),
-                      content: Wrap(children: [
-                        Column(
-                          children: [
-                            TextField(
-                              controller: test_readinp,
-                              decoration: const InputDecoration(
-                                border: OutlineInputBorder(
-                                    borderSide: BorderSide(
-                                  color: Colors.amber,
-                                )),
-                              ),
-                              style: const TextStyle(
-                                color: Colors.amber,
-                              ),
-                            ),
-                            TextField(
-                              controller: test_setout,
-                              enabled: enableflag,
-                            ),
-                            TextField(
-                              controller: test_outstate,
-                              enabled: enableflag,
-                            ),
-                          ],
-                        ),
-                      ]),
-                      actions: [
-                        ElevatedButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            child: Text("Back")),
-                        ElevatedButton(
-                            onPressed: () {
-                              test(test_readinp, test_setout, test_outstate);
-                            },
-                            child: Text("Go"))
-                      ],
-                    );
-                    showDialog(context: context, builder: (context) => alert);
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return StatefulBuilder(builder: (context, setState) {
+                            return AlertDialog(
+                              title: Container(
+                                  margin: EdgeInsets.fromLTRB(20, 5, 20, 1),
+                                  child: Text('Enter Custom test Inputs')),
+                              content: Wrap(children: [
+                                Column(
+                                  children: [
+                                    TextField(
+                                      controller: test_readinp,
+                                      decoration: const InputDecoration(
+                                        border: OutlineInputBorder(
+                                            borderSide: BorderSide(
+                                          color: Colors.amber,
+                                        )),
+                                      ),
+                                      style: const TextStyle(
+                                        color: Colors.amber,
+                                      ),
+                                    ),
+                                    Visibility(
+                                      visible: test_enableflag,
+                                      child: TextField(
+                                        controller: test_setout,
+                                        enabled: true,
+                                      ),
+                                    ),
+                                    Visibility(
+                                      visible: test_enableflag,
+                                      child: TextField(
+                                        controller: test_outstate,
+                                        enabled: true,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ]),
+                              actions: [
+                                ElevatedButton(
+                                    onPressed: () {
+                                      test_readinp.clear();
+                                      test_enableflag = false;
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text("Back")),
+                                ElevatedButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        test_enableflag = true;
+                                      });
+                                      test(test_readinp, test_setout,
+                                          test_outstate);
+                                    },
+                                    child: Text("Go"))
+                              ],
+                            );
+                          });
+                        });
                   }),
             ),
           ],
@@ -192,13 +228,10 @@ class _OptionsMenuState extends State<OptionsMenu> {
     );
   }
 
-  test(TextEditingController readinp, TextEditingController setout,
+  void test(TextEditingController readinp, TextEditingController setout,
       TextEditingController outstate) {
     String userinput = readinp.text;
-    print(userinput);
-    setState(() {
-      enableflag = true;
-    });
+
     try {
       String exitstate = widget.DFAobj.testInput(userinput.split(''));
       bool ans = widget.DFAobj.isAcceptingState(exitstate);
@@ -213,12 +246,12 @@ class _OptionsMenuState extends State<OptionsMenu> {
   void step_wise(TextEditingController step_readinp,
       TextEditingController step_setout, TextEditingController step_outstates) {
     String userinput = step_readinp.text;
+
     try {
       List<String> returnval =
           widget.DFAobj.testStepwiseInput(userinput.split(''));
       step_setout.text = "The custom input visits these states :-";
       step_outstates.text = returnval.join(' ');
-      print(returnval);
     } catch (e) {
       step_setout.text = "Invalid Output";
       step_outstates.text = "No states visited";
